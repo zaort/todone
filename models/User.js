033fba9fb426ffc598c046d64abab6872dd242d4
ThelmaRivas <thelma.rivas00@gmail.com>
2024-01-02 19:53:24 -0600

    Init User model
    Define user model with the necessary columns and its properties

--- models/User.js
+++ models/User.js
@@ -9,3 +9,30 @@ class User extends Model {
     }
 }
 
+User.init(
+    {
+        id: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+            primaryKey: true,
+            autoIncrement: true
+        },
+        username: {
+            type: DataTypes.STRING,
+            allowNull: false
+        },
+        email: {
+            type: DataTypes.STRING,
+            allowNull: false,
+            unique: true,
+            validate: {
+                isEmail: true
+            }
+        },
+        password: {
+            type: DataTypes.STRING,
+            allowNull: false,
+        },
+    },
+)
+       
