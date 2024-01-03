eeff881ca5605d42216ae74c8ec8045ca64a8901
ThelmaRivas <thelma.rivas00@gmail.com>
2024-01-02 19:56:19 -0600

    Add hooks
    define hooks that will be executed before creating or updating a user, using the bcrypt library to hash the user's password.

--- models/User.js
+++ models/User.js
@@ -34,5 +34,23 @@ User.init(
             allowNull: false,
         },
     },
-)
-       
+    {
+        hooks: {
+            async beforeCreate(newUserData) {
+                newUserData.password = await bcrypt.hash(newUserData.password, 10);
+                return newUserData;
+            },
+            async beforeUpdate(updatedUserData) {
+                updatedUserData.password = await bcrypt.hash(updatedUserData.password, 10);
+                return updatedUserData;
+            }
+        },
+        sequelize,
+        timestamps: false,
+        freezeTableName: true,
+        underscored: true,
+        modelName: 'user'
+    }
+);
+
+module.exports = User     
